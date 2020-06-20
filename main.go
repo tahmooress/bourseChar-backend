6d625dd19df756cb1fe40e4dfa761d58e3a15bc0
tahmooress <tahmoores.nassaj@gmail.com>
2020-06-20 16:05:27 +0430

    first fork commit version

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
@@ -17,29 +19,49 @@ func main() {
 	origin := handlers.AllowedOrigins([]string{"*"})
 	headers := handlers.AllowedHeaders([]string{"X-Request-With", "Content-Type", "Authorization"})
 	r.HandleFunc("/submit", submitHandler).Methods("POST")
+	r.HandleFunc("/upload", uploadHandler).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(headers, method, origin)(r)))
 }
 
 func submitHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("recieved")
 	p, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
-	var node []Node
+	var node []*Node
 	err = json.Unmarshal(p, &node)
 	if err != nil {
 		fmt.Println(err)
 	}
-	s := generateHTML(node[1])
+
+	for i := range node {
+		fmt.Println(i, *node[i])
+	}
+	f, err := os.Create("gnerated.html")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	generateHTML(node, f)
 	w.Write([]byte("yes"))
 	fmt.Println(node)
-	fmt.Println(s)
 }
 
+//Atribute is some type
+type Atribute struct {
+	ID     string `json:"id"`
+	Class  string `json:"class"`
+	Link   string `json:"href"`
+	src    string `json:"src"`
+	Target string `json:"target"`
+}
+
+//Node is some other type
 type Node struct {
 	NodeElement string   `json:"nodeName"`
 	NodeContent string   `json:"nodeContent"`
-	NodeAtr     []string `json:"nodeAtr"`
+	NodeAtr     Atribute `json:"nodeAtr"`
 	NodeChild   []*Node  `json:"nodeChild"`
 }
 
@@ -50,10 +72,77 @@ type Node struct {
 // 	</>section
 // `)
 
-func generateHTML(node Node) string {
-	var a string
-	if node.NodeElement == "p" {
-		a = fmt.Sprintf("<%s %v>%s</%s>", node.NodeElement, node.NodeAtr, node.NodeContent, node.NodeElement)
+func generateHTML(node []*Node, file *os.File) {
+	for _, element := range node {
+		s := fmt.Sprintf("<%s ", element.NodeElement)
+		_, err := file.WriteString(s)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if (Atribute{}) != element.NodeAtr {
+			s = fmt.Sprintf("%s %s %s %s %s>", element.NodeAtr.Class, element.NodeAtr.ID, element.NodeAtr.Link, element.NodeAtr.src, element.NodeAtr.Target)
+			fmt.Println(s)
+			_, err = file.WriteString(s)
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			s = ">"
+			_, err = file.WriteString(s)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		s = fmt.Sprintf("%s", element.NodeContent)
+		_, err = file.WriteString(s)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for len(element.NodeChild) > 0 {
+			generateHTML(element.NodeChild, file)
+			element.NodeChild = element.NodeChild[1:len(element.NodeChild)]
+		}
+		s = fmt.Sprintf("</%s>", element.NodeElement)
+		_, err = file.WriteString(s)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
-	return a
+}
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	imageName, err := fileUpload(r)
+	var response Result
+	if err != nil {
+		// http.Error(w, "Invalid Data", http.StatusBadRequest)
+		response.Res = ""
+		response.Er = err.Error()
+	}
+	w.Header().Set("Content-Type", "application/json")
+	response.Res = imageName
+	fmt.Println(imageName, "image name is here", response)
+	json.NewEncoder(w).Encode(response)
+}
+
+func fileUpload(r *http.Request) (string, error) {
+	r.ParseMultipartForm(32 << 20)
+	file, handler, err := r.FormFile("image")
+	fmt.Println(file, handler)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+	f, err := os.OpenFile("static/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+	io.Copy(f, file)
+	fn := "static/" + handler.Filename
+	return fn, nil
+}
+
+type Result struct {
+	Res string `json:"result"`
+	Er  string `json:"err"`
 }
